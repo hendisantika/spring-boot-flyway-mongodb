9503954b47481a9dc904f98911965d23f880b1ef
hendisantika <hendisantika@yahoo.co.id>
2025-01-02 13:35:19 +0700

    Add User Entity🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootflywaymongodb/entity/User.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.springbootflywaymongodb.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-flyway-mongodb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/01/25
+ * Time: 13.34
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "users")
+public class User {
+    @Id
+    private UUID id;
+    private String name;
+    private String email;
+}
