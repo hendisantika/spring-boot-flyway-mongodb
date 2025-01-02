d5ce70973b6ce62524e7a8c64bd6423428c822af
hendisantika <hendisantika@yahoo.co.id>
2025-01-02 13:40:07 +0700

    Add Init Data🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/flywaymongodb/SpringBootFlywayMongodbApplication.java
+++ src/main/java/id/my/hendisantika/flywaymongodb/SpringBootFlywayMongodbApplication.java
@@ -1,8 +1,16 @@
 package id.my.hendisantika.flywaymongodb;
 
+import id.my.hendisantika.flywaymongodb.entity.User;
+import id.my.hendisantika.flywaymongodb.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import java.util.UUID;
+
+@Slf4j
 @SpringBootApplication
 public class SpringBootFlywayMongodbApplication {
 
@@ -10,4 +18,17 @@ public class SpringBootFlywayMongodbApplication {
         SpringApplication.run(SpringBootFlywayMongodbApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner demo(UserRepository userRepository) {
+        return (args) -> {
+            log.info("Inserting data...");
+            userRepository.save(new User(UUID.randomUUID(), "Itadori Yuji", "yuji@yopmail.com"));
+            userRepository.save(new User(UUID.randomUUID(), "Megumi Fushiguro", "megumi@yopmail.com"));
+            userRepository.save(new User(UUID.randomUUID(), "Naobara Kugisaki", "naobara@yopmail.com"));
+            userRepository.save(new User(UUID.randomUUID(), "Gojo Satoru", "gojo@yopmail.com"));
+            userRepository.save(new User(UUID.randomUUID(), "Geto Suguru", "geto@yopmail.com"));
+            userRepository.save(new User(UUID.randomUUID(), "Maki Zenin", "maki@yopmail.com"));
+            userRepository.save(new User(UUID.randomUUID(), "Mai Zenin", "mai@yopmail.com"));
+        };
+    }
 }
