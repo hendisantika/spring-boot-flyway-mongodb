7a6a46da40e2b0c7f082594be99a87dde78f5302
hendisantika <hendisantika@yahoo.co.id>
2025-01-02 13:40:51 +0700

    Add findAll in UserController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/flywaymongodb/controller/UserController.java
+++ src/main/java/id/my/hendisantika/flywaymongodb/controller/UserController.java
@@ -1,10 +1,14 @@
 package id.my.hendisantika.flywaymongodb.controller;
 
+import id.my.hendisantika.flywaymongodb.entity.User;
 import id.my.hendisantika.flywaymongodb.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +26,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
     private final UserRepository userRepository;
+
+    @GetMapping
+    public List<User> findAll() {
+        log.info("Fetching all users");
+        return userRepository.findAll();
+    }
 }
