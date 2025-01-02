1f89df94434c511e4d4f21eade1f82e4e3174087
hendisantika <hendisantika@yahoo.co.id>
2025-01-02 13:30:31 +0700

    Create Spring Boot🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootflywaymongodb/SpringBootFlywayMongodbApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootflywaymongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlywayMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFlywayMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootflywaymongodb/SpringBootFlywayMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootflywaymongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFlywayMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
